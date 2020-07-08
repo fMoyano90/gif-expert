4abc723049c79819320511536c66cabce4bb5520
Felipe <f.moyano90@gmail.com>
2020-07-08 00:06:11 -0400

    Docs agregados

---
+++ docs/precache-manifest.7bc5036834d805a7982217644eaf5847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040e18d4b7db70d1d507bb0b519d16f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "233d8f537f304cf3d5b3",
+    "url": "/static/css/main.45e2ce2f.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233d8f537f304cf3d5b3",
+    "url": "/static/js/main.32cf6b5e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
